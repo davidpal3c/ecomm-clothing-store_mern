dccc72192299dd3a67467416c40eda9b4b8ab00e
davidpal3c <davidpal3c@gmail.com>
2024-11-10 17:16:55 -0700

    implemented login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -65,7 +65,33 @@ export const signup = async (req, res) => {
 
 
 export const login = async (req, res) => {
-    res.send("Login route called")
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({email})
+
+        if(user && (await user.comparePassword(password))){
+            const {accessToken, refreshToken} =  generateTokens(user._id)
+
+            await storeRefreshToken(user._id, refreshToken)
+            setCookies(res, accessToken, refreshToken)
+            
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email, 
+                role: user.role,
+            });
+        }
+        else {
+            res.status(401).json({ message: "Invalid email or password"});
+        }
+
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({})
+    }
+
+    // res.send("Login route called")
 };
 
 export const logout = async (req, res) => {
