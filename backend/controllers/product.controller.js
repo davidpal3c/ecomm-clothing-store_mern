89f538cbe995993b33285bf0fc166950a0a5e613
David <davidpal3c@gmail.com>
2024-12-24 16:47:03 -0700

    featured and create products routes

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,6 @@
 import Product from "../models/product.model.js";
+import { redis } from "../lib/redis.js";    
+import cloudinary from "../lib/cloudinary.js";
 
 
 export const getAllProducts = async (req, res) => {
@@ -15,8 +17,51 @@ export const getAllProducts = async (req, res) => {
 
 export const getFeaturedProducts = async (req, res) => {
     try {
+        let featuredProducts = await redis.get("featured_products")
+        if (featuredProducts) {
+            return res.json(JSON.parse(featuredProducts));  
+        }
+
+        // if not in redis, fetch from mongoDb
+        // .lean() returns a plain JavaScript object instead of a mongoose doc (good for performance)
+        featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+        if (!featuredProducts) {
+            return res.status(404).json({ message: "Featured products not found" });
+        }
+
+        // store in redis
+        await redis.set("featured_products", JSON.stringify(featuredProducts));  
         
+        res.json(featuredProducts);
     } catch (error) {
-        
+        console.log("Error in getFeaturedProducts controller", error.message);
+        res.status(500).json({ message: "Server Error", error: error.message });        
     }
 };    
+
+export const createProduct = async (req, res) => {
+    try {
+        const { name, description, price, image, category } = req.body;
+        
+        let cloudinaryResponse = null;
+
+        if (image) {
+            cloudinaryResponse = await cloudinary.uploader.upload(image, {folder: "products"});
+        }
+
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "", 
+            category,
+        });
+
+        res.status(201).json(product);
+
+    } catch (error) {
+        console.log("Error in createProduct controller", error.message);
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+};
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
+import { createProduct } from "../controllers/product.controller.js";
+
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
 
 export default router;
