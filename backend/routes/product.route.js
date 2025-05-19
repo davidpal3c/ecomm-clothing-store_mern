4f7fcd96002564e243e1ec9561c0da8643e2ca41
David <davidpal3c@gmail.com>
2025-05-19 09:07:13 -0600

    product-controller: updated and added featureProduct routes)

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -11,7 +11,6 @@ import {
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 // import { createProduct } from "../controllers/product.controller.js";
 
-
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
